5169a563613cf936612f798facaca99a426a8c32
LenOfAl <allennellasorry@gamil.com>
2023-08-13 21:46:10 +0530

    added requirements.txt

--- gmail.py
+++ gmail.py
@@ -3,9 +3,7 @@ import pickle
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
 
-import os
 os.chdir(os.getcwd()+'/Roomba')
 
 keywords = []
